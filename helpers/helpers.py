c9fb26d7c92ca105744fdbe304ecd6a5c2c262f8
Mikolaj <miki.pacman@gmail.com>
2020-08-19 19:08:42 +0200

    init

---
+++ helpers/helpers.py
@@ -0,0 +1,48 @@
+import retro
+import time
+import numpy as np
+from PIL import Image
+import cv2
+import soundfile as sf
+import tempfile
+import moviepy.editor as mpe
+
+
+def render_video(env, output):
+    # env = retro.make(game='SonicTheHedgehog-Genesis')
+    env.reset()
+
+    sound = np.array([])
+    frames = []
+
+    for _ in range(1000):
+        obs, rew, done, info = env.step(env.action_space.sample())
+
+        sound = np.append(sound, env.em.get_audio()[:, 0])
+        frames.append(cv2.cvtColor(obs, cv2.COLOR_RGB2BGR))
+
+        if done:
+            obs = env.reset()
+            break
+
+    rate = env.em.get_audio_rate()
+    env.close()
+
+    with tempfile.TemporaryDirectory(dir="/tmp") as temp:
+        fps =  len(frames) / (len(sound) / rate)
+        writer = cv2.VideoWriter(temp + '/tmp_vid.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 
+                                 fps, (obs.shape[1], obs.shape[0]))
+
+        for frame in frames:
+            writer.write(frame)
+
+        sf.write(temp + '/tmp_sound.flac', sound / (max(sound) * 4) , int(rate))
+
+        cv2.destroyAllWindows()
+        writer.release()
+
+        my_clip = mpe.VideoFileClip(temp + '/tmp_vid.mp4')
+        audio_background = mpe.AudioFileClip(temp + '/tmp_sound.flac')
+        final_clip = my_clip.set_audio(audio_background)
+        final_clip.write_videofile(output, logger=None)
+
